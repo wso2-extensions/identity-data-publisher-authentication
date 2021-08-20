27dc65287450e739dacbd5396bc2215a4efe0392
Dehami Koswatte <dehami.deshan@gmail.com>
2021-08-20 12:50:04 +0530

    Fix tenant domain null in audit logs for federated IDPs

--- components/org.wso2.carbon.identity.data.publisher.authentication.audit/src/main/java/org/wso2/carbon/identity/data/publisher/authentication/audit/AuthenticationAuditLoggerUtils.java
+++ components/org.wso2.carbon.identity.data.publisher.authentication.audit/src/main/java/org/wso2/carbon/identity/data/publisher/authentication/audit/AuthenticationAuditLoggerUtils.java
@@ -144,6 +144,9 @@ public class AuthenticationAuditLoggerUtils {
             if (localIDPData != null) {
                 tenantDomain = localIDPData.getUser().getTenantDomain();
             }
+            if (StringUtils.isBlank(tenantDomain)) {
+                tenantDomain = context.getTenantDomain();
+            }
         }
         return tenantDomain;
     }
